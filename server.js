2659bc6c9328be5aac4f4fe6b61bdc16a4f8354b
Les Carbonaro <lcarbonaro@gmail.com>
2016-05-24 12:40:38 +0000

    update

--- server.js
+++ server.js
@@ -5,10 +5,34 @@ var server = http.createServer(handleRequest);
 
 server.listen(process.env.PORT);
 
-function handleRequest(req,res) {
-    
+function handleRequest(req, res) {
+
     var route = url.parse(req.url).pathname;
-    
+
+
+    var inp = isNaN(parseInt(route)) ? route : parseInt(route) * 1000;
+
+    var date = new Date(inp);
+    var o;
+
+    if (isValidDate(date)) {
+        var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        o = {
+            "unix": parseInt(date.getTime() / 1000),
+            "natural": month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+        };
+    }
+    else {
+        o = {
+            "unix": null,
+            "natural": null
+        };
+    }
+
+    res.write(JSON.stringify(o));
+    res.end();
+
+    /*
     switch(route) {
         
         case '/':
@@ -26,4 +50,17 @@ function handleRequest(req,res) {
             res.end();
             break;
     }
+    */
+}
+
+// from: http://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+function isValidDate(d) {
+    var rv = false;
+    if (Object.prototype.toString.call(d) !== "[object Date]") {
+        rv = false;
+    }
+    else {
+        rv = !isNaN(d.getTime());
+    }
+    return rv;
 }
